76b383e52ed27d4bcfe8f925eff687b290704757
andrew beatrice <abeatrice.mail@gmail.com>
2021-02-15 13:43:36 -0800

    wip icons

--- resources/js/Ab/SortButton.vue
+++ resources/js/Ab/SortButton.vue
@@ -5,7 +5,7 @@
                 <slot></slot>
             </div>
             <div class="flex-none ml-1">
-                <svg v-if="direction == 'ASC'" class="h-4 w-4" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M8 7l4-4m0 0l4 4m-4-4v18"/></svg>
+                <icon-arrow-narrow-up v-if="direction == 'ASC'" />
                 <svg v-else-if="direction == 'DESC'" class="h-4 w-4" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M16 17l-4 4m0 0l-4-4m4 4V3"/></svg>
                 <svg v-else class="h-4 w-4" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M7 16V4m0 0L3 8m4-4l4 4m6 0v12m0 0l4-4m-4 4l-4-4"/></svg>
             </div>
@@ -14,7 +14,12 @@
 </template>
 
 <script>
+    import IconArrowNarrowUp from '../Icon/IconArrowNarrowUp'
+
     export default {
+        components: {
+            IconArrowNarrowUp,
+        },
         props: {
             direction: {
                 type: String,
---
+++ resources/js/Icon/IconArrowNarrowUp.vue
@@ -0,0 +1,14 @@
+<template>
+    <icon v-bind="$props">
+        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M8 7l4-4m0 0l4 4m-4-4v18"/>
+    </icon>
+</template>
+
+<script>
+    import Icon from './Icon'
+
+    export default {
+        props: ['size'],
+        components: {Icon},
+    }
+</script>
